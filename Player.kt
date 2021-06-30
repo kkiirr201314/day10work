9ea5cb1cafce7897c7a34a29ec35dbbdfdecf96b
“kkiirr201314” <qq255073@gmail.com>
2021-07-01 07:39:21 +0800

    c12-8

--- Player.kt
+++ Player.kt
@@ -1,6 +1,9 @@
+import java.util.*
+
 class Player {
 
     val name = "madrigal"
+        get() = field.capitalize()
 
     fun castFireball(numFireball: Int = 2) =
         println("橫空變出一杯火球" + "(x$numFireball)")
