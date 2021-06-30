5feee7d954acf74763d8bf031e4d971cbdaf82f5
“kkiirr201314” <qq255073@gmail.com>
2021-07-01 07:58:42 +0800

    c12-18(fail)

--- game.kt
+++ game.kt
@@ -22,22 +22,7 @@ private fun printPlayerStatus(
     println("光環: $auraColor" + "     走運嗎?${if (isBlessed) "是的" else "否，他很背"}")
     println("$name $healthStatus")
 }
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
-        if (isBlessed && healthPoints > 50 || isImmortal) "綠色" else "無光環"
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints) {
-        100 -> "健康狀態極佳"
-        in 90..99 -> "有一些小擦傷"
-        in 75..89 -> if (isBlessed) {
-            "雖有一些傷口，但恢復很快"
-        } else {
-            "有一些傷口"
-        }
-        in 15..74 -> "嚴重受傷"
-        //顯示玩家狀態
-        else -> "情況不妙"
-    }
 private fun castFireBall(numFireballs: Int = 2): Int {
     print("FireBall杯數(x$numFireballs)")
     return numFireballs
