13ffe6d9f606d48b86db3a3b8c9adaa4d6b0589d
“kkiirr201314” <qq255073@gmail.com>
2021-07-01 07:41:34 +0800

    c12-9(fail)

--- Player.kt
+++ Player.kt
@@ -4,6 +4,9 @@ class Player {
 
     val name = "madrigal"
         get() = field.capitalize()
+        set(value){
+            field = value.trim()
+        }
 
     fun castFireball(numFireball: Int = 2) =
         println("橫空變出一杯火球" + "(x$numFireball)")
