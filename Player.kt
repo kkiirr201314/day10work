969530084e6cf35322c7c5bb926311c4275c855b
“kkiirr201314” <qq255073@gmail.com>
2021-07-01 08:00:01 +0800

    c12-19(fail)

--- Player.kt
+++ Player.kt
@@ -11,6 +11,22 @@ class Player {
     var isBlessed = true
     private val isImmortal = false
 
+    private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
+        if (isBlessed && healthPoints > 50 || isImmortal) "綠色" else "無光環"
+
+    private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+        when (healthPoints) {
+            100 -> "健康狀態極佳"
+            in 90..99 -> "有一些小擦傷"
+            in 75..89 -> if (isBlessed) {
+                "雖有一些傷口，但恢復很快"
+            } else {
+                "有一些傷口"
+            }
+            in 15..74 -> "嚴重受傷"
+            //顯示玩家狀態
+            else -> "情況不妙"
+        }
     fun castFireball(numFireball: Int = 2) =
         println("橫空變出一杯火球" + "(x$numFireball)")
 }
